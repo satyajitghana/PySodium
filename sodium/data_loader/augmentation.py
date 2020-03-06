f5cd320a5b397f1e86d9edb9376ebe3ab7b918fe
Satyajit Ghana <satyajitghana7@gmail.com>
2020-03-06 20:18:12 +0530

    add augmentation to CIFAR10

--- sodium/data_loader/augmentation.py
+++ sodium/data_loader/augmentation.py
@@ -19,16 +19,28 @@ class AugmentationFactoryBase(abc.ABC):
 class MNISTTransforms(AugmentationFactoryBase):
 
     def build_train(self):
-        return T.Compose([T.ToTensor(), T.Normalize((0.1307,), (0.3081,))])
+        return T.Compose([
+            T.ToTensor(),
+            T.Normalize((0.1307,), (0.3081,))])
 
     def build_test(self):
-        return T.Compose([T.ToTensor(), T.Normalize((0.1307,), (0.3081,))])
+        return T.Compose([
+            T.ToTensor(),
+            T.Normalize((0.1307,), (0.3081,))])
 
 
 class CIFAR10Transforms(AugmentationFactoryBase):
 
     def build_train(self):
-        return T.Compose([T.ToTensor(), T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+        return T.Compose([
+            T.RandomCrop(32, padding=4),
+            T.RandomHorizontalFlip(),
+            T.ToTensor(),
+            T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])
 
     def build_test(self):
-        return T.Compose([T.ToTensor(), T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
+        return T.Compose([
+            T.ToTensor(),
+            T.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+        ])
